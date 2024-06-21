2540b4d8ccf202018524815e2af8de811fc4aa0b
uncpleo <leonardocqtc@gmail.com>
2024-06-21 11:18:53 -0500

    segundo commit

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -6,17 +6,25 @@ const EmployeeList = ({ setEmployeeToEdit }) => {
 
   return (
     <div>
-      {state.map(employee => (
-        <div key={employee.id}>
-          <h3>{employee.name}</h3>
-          <p>{employee.address}</p>
-          <p>{employee.city}</p>
-          <button onClick={() => setEmployeeToEdit(employee)}>Editar</button>
+      <table className='crd'>
+        <tr className='titulo_T'>
+          <th>Nombre</th>
+          <th>Dirección</th>
+          <th>Ciudad</th>
+          <th>Opciones</th>
+        </tr>
+        {state.map(employee => (
+        <tr className='contenido_T' key={employee.id}>
+          <td>{employee.name}</td>
+          <td><p>{employee.address}</p></td>
+          <td><p>{employee.city}</p></td>
+          <td><button onClick={() => setEmployeeToEdit(employee)}>Editar</button>
           <button onClick={() => dispatch({ type: 'DELETE_EMPLOYEE', payload: employee.id })}>
             Eliminar
-          </button>
-        </div>
+          </button></td>
+        </tr>
       ))}
+      </table>
     </div>
   );
 };
